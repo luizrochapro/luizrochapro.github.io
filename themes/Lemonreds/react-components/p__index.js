76fdcfc4c17c616c41ed16e9a811bb43fa633300
Luiz <luizgrj@gmail.com>
2023-02-21 21:04:55 -0300

    tema

---
+++ themes/Lemonreds/react-components/p__index.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[6],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),c=t.n(a),o=t("Ty5D"),p=()=>c.a.createElement(o["a"],{to:"/comp"});n["default"]=p}}]);
